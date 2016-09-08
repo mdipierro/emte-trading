83c35a0dbbab8e1f0e1396c7370809ea968036ff
stevetran <htran15@mail.depaul.edu>
2016-09-08 18:12:19 -0500

    add document

--- modules/robot_trader.py
+++ modules/robot_trader.py
@@ -1,6 +1,15 @@
 import random, hmac, urllib, time, optparse
 
 def robot_order(price,owner='1,2'):
+    """
+    submits buy and sell orders randomly
+    * Order price is fluctuated around quote price
+
+    :param float price: The price gotten back from QuoteHandler.get
+    :param str owner: a group/individual owner. Default is random owner
+    :return: order information
+    :rtype: str
+    """
     owners=str(owner).split(',')
     owner=owners[random.randint(0,len(owners)-1)]
     i=random.randint(0,3)
@@ -10,7 +19,7 @@ def robot_order(price,owner='1,2'):
         if p<=20: p=20+abs(random.gauss(0,20))
     elif i is 3:
         p=p+abs(random.gauss(0,10)) # dollars
-        if p>=180: p=180-abs(random.gauss(0,20))        
+        if p>=180: p=180-abs(random.gauss(0,20))
     p = 0.01*int(p*100)
     n=random.randint(1,1000)   # shares
     if i==0: o='%s:buy %i@%s' % (owner,n,0)
@@ -20,6 +29,13 @@ def robot_order(price,owner='1,2'):
     return o
 
 def start_robot(url,owner=0,hmac_key='secret',wait_time=1):
+    """
+    Starts robot trader
+
+    :param str url: the websocket url endpoint
+    :param str hmac_key: the secrect key to encrypt signature
+    :param int wait_time: the interval to submit new order
+    """
     while True:
         t0 = time.time()
         price = float(urllib.urlopen(url+'/quote').read())
@@ -57,8 +73,3 @@ if __name__=='__main__':
                       help='time between two trades in seconds')
     (options, args) = parser.parse_args()
     start_robot('http://127.0.0.1:%s' % options.port,owner=options.owner,hmac_key=options.hmac_key,wait_time=options.wait_time)
-        
-
-        
-    
-    
